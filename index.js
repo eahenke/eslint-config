35fd07db1c723ada5855273dc4dbd67bbc16f948
Eric Henke <eahenke@gmail.com>
2019-11-02 13:39:14 -0700

    correct multiple config setup

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const jsConfig = require('./eslint');
-const typescriptConfig = require('./eslint-typescript');
-
 module.exports = {
-    javascript: jsConfig,
-    typescript: typescriptConfig
+    extends: ['./rules/base.js'],
+    rules: {}
 };
